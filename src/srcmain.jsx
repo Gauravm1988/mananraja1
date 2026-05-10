117dcb0aa9e8c10f2dfa56ad037ced5c4a45e769
Gauravm1988 <gaurav.viit@gmail.com>
2026-05-10 23:04:40 +0530

    Update srcmain.jsx

--- src/srcmain.jsx
+++ src/srcmain.jsx
@@ -1,6 +1,10 @@
-importReactfrom'react'
-importReactDOMfrom'react-dom/client'
-importAppfrom'./App'
-import'./index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>,
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
