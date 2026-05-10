38e51ced834c736a0a1f3d7f3a16b63d9c761ef6
Gauravm1988 <gaurav.viit@gmail.com>
2026-05-10 22:41:45 +0530

    Add files via upload

---
+++ src/srcApp.jsx
@@ -0,0 +1,2 @@
+exportdefaultfunctionApp(){return(<divstyle={{padding:'40px',textAlign:'center'}}><h1>MananrajaRealty</h1><p>LuxuryPropertyConsultant•Pune</p></div>)
+}
---
+++ src/srcmain.jsx
@@ -0,0 +1,6 @@
+importReactfrom'react'
+importReactDOMfrom'react-dom/client'
+importAppfrom'./App'
+import'./index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>,
+)
